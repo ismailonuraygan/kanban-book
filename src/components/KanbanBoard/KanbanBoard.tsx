5cb2ab21361f4f9b8db3be7c9124e85d84dc35be
onur <ionuraygan@gmail.com>
2023-01-07 01:49:28 +0300

    Adding Columns into Kanban board

--- src/components/KanbanBoard/KanbanBoard.tsx
+++ src/components/KanbanBoard/KanbanBoard.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
-import Columns from './Columns/Columns';
-import styles from './index.module.scss';
+import React, { useEffect, useState } from "react";
+import Columns from "./Columns/Columns";
+import styles from "./index.module.scss";
 
 const KanbanBoard = () => {
+  const [authorId, setAuthorId] = useState("");
+  const arr = [1, 2, 3];
+
   return (
     <div className={styles.wrapper}>
-      <form action=""></form>
-      <Columns />
+      <div className={styles.formWrapper}>
+        <h1>Lorem Board</h1>
+        <h4>books of</h4>
+        <form action="" className={styles.form}>
+          <input
+            type="text"
+            placeholder="Enter Author Id"
+            value={authorId}
+            onChange={(e) => setAuthorId(e.target.value)}
+          />
+          <button>submit</button>
+        </form>
+      </div>
+      <div className={styles.columnsWrapper}>
+        {arr.map((_, i) => (
+          <Columns key={i} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default KanbanBoard
+export default KanbanBoard;
