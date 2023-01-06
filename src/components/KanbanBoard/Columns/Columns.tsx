a16520f35175dee6f0e244fbdeb9c42a25ca9160
onur <ionuraygan@gmail.com>
2023-01-07 01:37:53 +0300

    Adding Card component into Columns

--- src/components/KanbanBoard/Columns/Columns.tsx
+++ src/components/KanbanBoard/Columns/Columns.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import styles from './index.module.scss';
+import React from "react";
+import Card from "./Card/Card";
+import styles from "./index.module.scss";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const Columns = () => {
-  return (
-    <div>Columns</div>
-  )
-}
+  const array = [1, 2, 3];
 
-export default Columns
+  return (
+    <div className={styles.columnsWrapper}>
+      <div className={styles.columnsHeader}>
+        <h1>1925</h1>
+        <MoreVertIcon className={styles.icon} />
+      </div>
+      {array.map((_, i) => (
+        <Card key={i} />
+      ))}
+    </div>
+  );
+};
+
+export default Columns;
