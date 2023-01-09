485e4f5fa4aa65fc248eee90b88dc832d482a4bc
onur <ionuraygan@gmail.com>
2023-01-09 15:36:59 +0300

    Fetch&pass data to children component as a prop

--- src/components/KanbanBoard/KanbanBoard.tsx
+++ src/components/KanbanBoard/KanbanBoard.tsx
@@ -1,13 +1,30 @@
 import React, { useEffect, useState } from "react";
 import Columns from "./Columns/Columns";
 import styles from "./index.module.scss";
+import { transformKanban } from "../../utils/utilities";
+import { getAuthor } from "../../pages/api";
 
 const KanbanBoard = () => {
-  const [authorId, setAuthorId] = useState("");
-  const arr = [1, 2, 3];
+  const [authorId, setAuthorId] = useState("OL26320A");
+  const [transformedBook, setTransformedBook] = useState({});
+
+  async function getBooks() {
+    const response = await getAuthor(authorId);
+    const newDoc = transformKanban(response);
+    /* console.log(newDoc, "newDoc") */
+    console.log(response, "pure");
+    /* console.log(data); */
+    setTransformedBook(newDoc);
+
+    /* console.log(transformedBook, "abc"); */
+  }
+
+  useEffect(() => {
+    getBooks();
+  }, []);
 
   return (
-    <div className={styles.wrapper}>
+    <>
       <div className={styles.formWrapper}>
         <h1>Lorem Board</h1>
         <h4>books of</h4>
@@ -19,14 +36,22 @@ const KanbanBoard = () => {
             onChange={(e) => setAuthorId(e.target.value)}
           />
           <button>submit</button>
+          
         </form>
       </div>
-      <div className={styles.columnsWrapper}>
-        {arr.map((_, i) => (
-          <Columns key={i} />
-        ))}
+      <div className={styles.wrapper}>
+        <div className={styles.columnsWrapper}>
+          {transformedBook &&
+            Object.keys(transformedBook).map((year, i) => (
+              <Columns
+                key={i}
+                year={year}
+                transformedBook={Object.values(transformedBook)[i]}
+              />
+            ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
