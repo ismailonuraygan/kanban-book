64c97b6796cf462d515690cabe08767a46ae66c5
onur <ionuraygan@gmail.com>
2023-01-09 15:41:38 +0300

    Receive book data as a prop and rendered it

--- src/components/KanbanBoard/Columns/Card/Card.tsx
+++ src/components/KanbanBoard/Columns/Card/Card.tsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.scss";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import LayersOutlinedIcon from '@mui/icons-material/LayersOutlined';
-import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
-const Card = () => {
+
+const Card = ({ book }) => {
+  const [open, setOpen] = useState(true);
+  const handleClick = () => {
+    setOpen(!open);
+  };
+  /* console.log(book); */
   return (
     <div className={styles.cardWrapper}>
-      <div className={styles.cardHeader}>
-        <h1>Card Header</h1>
-      </div>
-      <div className={styles.cardContent}>
-        <div className={styles.editions}>6 Editions</div>
-        <p>First Published: 1925</p>
-      </div>
-      <div className={styles.cardFooter}>
-        <div className={styles.pages}>
-          <LayersOutlinedIcon style={{ width: '14px' }} />
-          <p>123 pages</p>
+      <button className={styles.cardHeader} onClick={() => handleClick()}>
+        <div className={open ? styles.titleOpen : styles.titleClose}>
+          {book.title}
         </div>
-        <div className={styles.readTime}>
-          <AccessTimeIcon style={{ width: '14px' }} />
-          <p>32 hours read time</p>
+      </button>
+      <div className={open ? styles.bodyOpen : styles.body}>
+        <div className={styles.content}>
+          <div className={styles.cardContent}>
+            <div className={styles.editions}>{book.edition_count} Editions</div>
+            <p>First Published: {book.first_publish_year}</p>
+          </div>
+          <div className={styles.cardFooter}>
+            <div className={styles.pages}>
+              <p>{book.number_of_pages_median} pages</p>
+            </div>
+            <div className={styles.readTime}>
+              <p>32 hours read time</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
