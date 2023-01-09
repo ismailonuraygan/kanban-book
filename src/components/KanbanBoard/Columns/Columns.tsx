88529e5f267ae79488bb85f97ed30bd1b29b50e6
onur <ionuraygan@gmail.com>
2023-01-09 15:46:07 +0300

    Receive data as a prop

--- src/components/KanbanBoard/Columns/Columns.tsx
+++ src/components/KanbanBoard/Columns/Columns.tsx
@@ -3,18 +3,24 @@ import Card from "./Card/Card";
 import styles from "./index.module.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 
-const Columns = () => {
+const Columns = ({ transformedBook, year }) => {
   const array = [1, 2, 3];
-
+  /* console.log(transformedBook) */
   return (
     <div className={styles.columnsWrapper}>
+      <div
+        className={styles.color}
+        style={{ backgroundColor: `#FA${year}` }}
+      ></div>
       <div className={styles.columnsHeader}>
-        <h1>1925</h1>
+        <h1>{year}</h1>
         <MoreVertIcon className={styles.icon} />
       </div>
-      {array.map((_, i) => (
-        <Card key={i} />
-      ))}
+      <div className={styles.cards}>
+        {transformedBook.map((book, i) => (
+          <Card key={i} book={book} />
+        ))}
+      </div>
     </div>
   );
 };
