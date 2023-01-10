43f99beea71cdb0984b576e9d89cc6d687db72af
onur <ionuraygan@gmail.com>
2023-01-10 21:24:36 +0300

    Updated loog

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import styles from "./index.module.scss";
+import Image from "next/image";
+import Logo from "../../assets/logo/brewLogo.png";
 
 const Header = () => {
   return (
     <div className={styles.wrapper}>
-      Header
+      <Image
+        src={Logo}
+        width={96}
+        height={48}
+        alt="Logo of Brew Interactive"
+        className={styles.logo}
+      />
     </div>
   );
 };
