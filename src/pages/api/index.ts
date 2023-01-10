24aa1c06d3e80882c8c4ebdbdd53beae849c9ae5
onur <ionuraygan@gmail.com>
2023-01-11 02:25:40 +0300

    Remove limit of data

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,5 +1,5 @@
 export const getAuthor = async (authorID: string) => {
-    const author = await fetch(`https://openlibrary.org/search.json?language=eng&author=${authorID}&limit=10`)
+    const author = await fetch(`https://openlibrary.org/search.json?language=eng&author=${authorID}`)
         .then(res => res.json())
         .then(res => res.docs);
     return author;
