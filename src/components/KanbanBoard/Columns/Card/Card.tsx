c65d08c2a468d71b53f498634164f09ca8478c3a
onur <ionuraygan@gmail.com>
2023-01-11 12:55:23 +0300

    Fix empty values

--- src/components/KanbanBoard/Columns/Card/Card.tsx
+++ src/components/KanbanBoard/Columns/Card/Card.tsx
@@ -24,13 +24,13 @@ const Card: FC<CardProps> = ({ book }) => {
       <div className={open ? styles.bodyOpen : styles.body}>
         <div className={styles.content}>
           <div className={styles.cardContent}>
-            <div className={styles.editions}>{book.edition_count} Editions</div>
+            {book.edition_count && <div className={styles.editions}>{book.edition_count} Editions</div>}
             <p>First Published: {book.first_publish_year}</p>
           </div>
           <div className={styles.cardFooter}>
             <div className={styles.pages}>
-              <LayersOutlinedIcon style={{ width: "14px" }} />
-              <p>{book.number_of_pages_median} pages</p>
+              {book.number_of_pages_median && <LayersOutlinedIcon style={{ width: "14px" }} />}
+              {book.number_of_pages_median && <p>{book.number_of_pages_median} pages</p>}
             </div>
             <div className={styles.readTime}>
               <AccessTimeIcon style={{ width: "14px" }} />
