c600f2de1e2ae808ff48a2599c4655582fd48abc
onur <ionuraygan@gmail.com>
2023-01-11 11:29:27 +0300

    Setting environment variables

--- next.config.js
+++ next.config.js
@@ -14,6 +14,10 @@ const nextConfig = {
     })
     return config
   },
+  env: {
+    NEXT_PUBLIC_BASE_URL: 'https://openlibrary.org',
+  },
+
 }
 
 module.exports = nextConfig
--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,5 +1,5 @@
 export const getAuthor = async (authorID: string) => {
-    const author = await fetch(`https://openlibrary.org/search.json?language=eng&author=${authorID}`)
+    const author = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/search.json?language=eng&author=${authorID}`)
         .then(res => res.json())
         .then(res => res.docs);
     return author;
